ebdb1268a3e64bdcb659f045252f39b4d3f2a615
Teapack1 <ondrej.major@gmail.com>
2024-02-17 23:15:11 +0100

    reqs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import subprocess
 
-print(subprocess.run("sqlite3 --version", shell=True))
+result = subprocess.run("sqlite3 --version", shell=True, capture_output=True, text=True)
+print(result.stdout.strip())
 
 print("Starting the FastAPI server...")
 subprocess.run("uvicorn fast_app:app --host 0.0.0.0 --port 7860", shell=True)
